2814f2c6411dc51f5c66e5b0517ffa9522a329bb
c h <carolinehen223@gmail.com>
2025-04-16 14:23:17 -0700

    completed exercise

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -5,6 +8,16 @@ import java.util.Scanner;
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.print("Please enter your full name: ");
+        String name = scanner.nextLine();
+        System.out.print("Please enter the date(MM/dd/yyyy): ");
+        String date = scanner.nextLine();
+        System.out.print("How many tickets would you like? ");
+        int tickets = scanner.nextInt();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/dd/yyyy");
+        LocalDate movieTime = LocalDate.parse(date,formatter);
+        System.out.println(tickets + " tickets reserved for " + movieTime + " under " + name);
 
     }
 }
